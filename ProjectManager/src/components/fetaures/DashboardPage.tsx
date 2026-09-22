14a410c59f91464d84ea45f1de833f22f62e76c0
Pontusdemon <Pontus.ling@gmail.com>
2026-09-22 11:10:40 +0200

    build 7

--- ProjectManager/src/components/fetaures/DashboardPage.tsx
+++ ProjectManager/src/components/fetaures/DashboardPage.tsx
@@ -5,14 +5,14 @@ const DashboardPage = () => {
 	return (
     <div>
         <div>
-            <div className="flex justify-between">
-                <div className="m-2">dahsboard headline</div>
+            <div className="m-2 flex flex-row justify-evenly">
+                <div className="">dahsboard headline</div>
                 <div className="m-2 text-sm text-muted-foreground">new project trigger</div>
             </div>
         </div>
         <Separator />
         <div>
-            <div className="flex flex-row gap-3 justify-between">
+            <div className="flex flex-row gap-3 justify-evenly">
                 <div className="m-4"> projects </div>
                 <Separator orientation="vertical" />
                 <div className="m-4"> open tasks </div>
@@ -23,7 +23,7 @@ const DashboardPage = () => {
             </div>
         </div>
         <Separator />
-        <div className="flex flex-row justify bewtween">
+        <div className="flex flex-row justify-evenly">
             <Card className="m-10">
                 project progress
             </Card>
